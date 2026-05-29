c843ab14043dce7f180845b7ea56534f11af3a1a
Monukumawa <monukumwat14@gmail.com>
2026-05-29 14:52:26 +0530

    Fixed CORS

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ const PORT = process.env.PORT || 4000
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: "https://ai-saas-image-generation-platform-9y4r-ie7foi6w0.vercel.app",
+  credentials: true
+}))
 await connectDB()
 
 app.use('/api/user' , userRouter)
